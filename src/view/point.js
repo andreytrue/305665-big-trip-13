0eb1d8c6a2e37ccc0aeab96260ccbf95def2ddf2
Andrey Demyanov <demyanov23@gmail.com>
2021-01-17 01:36:26 +0300

    Исправляет показ картинок в статусах

--- src/view/point.js
+++ src/view/point.js
@@ -28,7 +28,7 @@ const createPointTemplate = (point) => {
       <div class="event">
         <time class="event__date" datetime="${start.format(`YYYY-MM-DD`)}">${start.format(`MMM DD`)}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${city}</h3>
       <div class="event__schedule">
